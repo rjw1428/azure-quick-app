6241399891b22e2d0d1cd5bfc16b77dd99f94b86
Ryan Wilk <rjw1428@gmail.com>
2023-12-27 21:31:58 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'quick-app';
+  selectedDate = signal('');
+  count = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log(`The count is ${this.count()}`)
+    })    
+  }
+
+  onClick(input: HTMLInputElement) {
+    console.log(input.value)
+    this.selectedDate.set(input.value) // set value directly
+    this.count.update(v => v + 1);
+  }
 }
